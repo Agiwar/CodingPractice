1affd115f8f1562784c75989ce2e7f9e61ec15f4
Agiwar <agiwar791005@gmail.com>
2024-06-17 00:07:17 -0500

    make some changes

--- Algorithm/array/min_stack.py
+++ Algorithm/array/min_stack.py
@@ -2,17 +2,21 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.min_stack = []
     
     def push(self, val: int) -> None:
         self.stack.append(val)
+        min_val = min(val, self.min_stack[-1] if self.min_stack else val)
+        self.min_stack.append(min_val)
         return None
     
     def pop(self) -> None:
         self.stack.pop()
+        self.min_stack.pop()
         return None
     
     def top(self) -> int:
         return self.stack[-1]
     
     def get_min(self) -> int:
-        return min(self.stack)
+        return self.min_stack[-1]
