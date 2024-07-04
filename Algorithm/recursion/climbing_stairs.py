5a1405fc67ef89c9aa43e44bfb89f39cc7a2ad3a
Jeffrey Li <jeffreyli@JFs-MacBook-Air.local>
2024-07-04 15:31:51 +0800

    add climbing_stairs.py file

---
+++ Algorithm/recursion/climbing_stairs.py
@@ -0,0 +1,26 @@
+import argparse
+
+
+def argparse_integer() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--int", type=int)
+    args = parser.parse_args()
+    return args
+
+
+def climb_stairs(n: int) -> int:
+    one, two = 1, 1
+
+    for _ in range(n - 1):
+        one, two = (one + two), one
+    
+    return one
+
+
+if __name__ == "__main__":
+    args = argparse_integer()
+    num_of_stairs = args.int
+    num_different_ways = climb_stairs(num_of_stairs)
+
+    # given n = 5, you may get 8
+    print(num_different_ways)
