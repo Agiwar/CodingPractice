6e12ece357bfbad6b8976bfbf710594a883b1748
Agiwar <agiwar791005@gmail.com>
2024-07-18 16:36:57 +0800

    implementation for merge sort

---
+++ Python/merge_sort.py
@@ -0,0 +1,27 @@
+import argparse
+from typing import List
+
+
+def argparse_list() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("given_arr", type=int, nargs="+")
+    args = parser.parse_args()
+    return args
+
+
+def merge_sort(arr: List[int]) -> List[int]:
+    if len(arr) <= 1:
+        return arr
+    
+    m = len(arr) // 2
+
+    left_half = arr[:m]
+    right_half = arr[m:]
+
+    return merge_sort(left_half), merge_sort(right_half)
+
+
+if __name__ == "__main__":
+    args = argparse_list()
+    arr = args.given_arr
+    print(merge_sort(arr))
