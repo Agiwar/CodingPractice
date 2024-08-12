8ae85c1eff67c2a1b0b227a118ad65dee73aa64d
Agiwar <agiwar791005@gmail.com>
2024-08-12 16:49:39 +0800

    implementation of BST Insert and Remove

--- Python/binary_search_tree.py
+++ Python/binary_search_tree.py
@@ -11,6 +11,9 @@ class TreeNode:
 
 
 def binary_search_tree(root: Optional[TreeNode], target: int) -> bool:
+    """
+    Check whether target value exists in the BST or not.
+    """
     if not root:
         return False
     
@@ -20,3 +23,55 @@ def binary_search_tree(root: Optional[TreeNode], target: int) -> bool:
         return binary_search_tree(root.left, target)
     else:
         return True
+
+
+def insert(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    """
+    Insert a new node into the BST.
+    """
+    if not root:
+        return TreeNode(val=val)
+    
+    if val > root.val:
+        root.right = insert(root.right, val)
+    elif val < root.val:
+        root.left = insert(root.left, val)
+
+    return root
+
+
+def find_min_node(root: Optional[TreeNode]) -> Optional[TreeNode]:
+    """
+    Find a min-value node in the BST, the min value is always in the left sub-tree
+    """
+    curr = root
+
+    while curr and curr.left:
+        curr = curr.left
+    
+    return curr
+
+
+def remove(root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+    """
+    Remvoe a specific node from a existing BST
+    """
+    if not root:
+        return None
+    
+    if val > root.val:
+        root.right = remove(root.right, val)
+    elif val < root.val:
+        root.left = remove(root.left, val)
+    else:
+        # target found, and check the current node is leaf or not
+        if not root.left:
+            return root.right
+        elif not root.right:
+            return root.left
+        else:
+            min_value_node = find_min_node(root.right)
+            root.val = min_value_node.val
+            root.right = remove(root.right, min_value_node.val)
+    
+    return root
