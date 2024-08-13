2d3fa3f0732754bc8d13433ad02b09e4fa8e7f06
Agiwar <agiwar791005@gmail.com>
2024-08-13 22:28:57 +0800

    implementation of DFS

---
+++ Python/depth_first_search.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def inorder(root: Optional[TreeNode]) -> None:
+    """
+    Visit left sub-tree fist, then visit root, and finally visit right sub-tree.
+    """
+    if not root:
+        return None
+    
+    inorder(root.left)
+    print(root.val)
+    inorder(root.right)
+
+
+def preorder(root: Optional[TreeNode]) -> None:
+    """
+    Visit parent root first, then visit left sub-tree, and finally visit right sub-tree.
+    """
+    if not root:
+        return None
+    
+    print(root.val)
+    preorder(root.left)
+    preorder(root.right)
+
+
+def postorder(root: Optional[TreeNode]) -> None:
+    """
+    Visit left sub-tree first, then visit right sub-tree, and finally visit root.
+    """
+    if not root:
+        return None
+    
+    postorder(root.left)
+    postorder(root.right)
+    print(root.val)
