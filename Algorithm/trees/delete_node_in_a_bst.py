59fc249c082d6c74b075219844c592f71489451b
Agiwar <agiwar791005@gmail.com>
2024-08-14 00:05:37 +0800

    use nested function not method

--- Algorithm/trees/delete_node_in_a_bst.py
+++ Algorithm/trees/delete_node_in_a_bst.py
@@ -5,15 +5,14 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def find_min_node(self, root: Optional[TreeNode]):
-        curr = root
-
-        while curr and curr.left:
-            curr = curr.left
-        
-        return curr
-
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+
+        def find_min_node(root: Optional[TreeNode]) -> Optional[TreeNode]:
+            curr = root
+            while curr and curr.left:
+                curr = curr.left
+            return curr
+        
         if not root:
             return None
         
@@ -27,8 +26,8 @@ class Solution:
             elif not root.right:
                 return root.left
             else:
-                min_value_node = self.find_min_node(root.right)
-                root.val = min_value_node.val
-                root.right = self.deleteNode(root.right, min_value_node.val)
-
+                min_val_node = find_min_node(root.right)
+                root.val = min_val_node.val
+                root.right = self.deleteNode(root.right, min_val_node.val)
+        
         return root
