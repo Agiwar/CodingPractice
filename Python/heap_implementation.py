6b04d1113e4a87d6f49db6a5288475db0e5d63ac
Agiwar <agiwar791005@gmail.com>
2024-08-30 23:43:06 +0800

    implementation of heapify

--- Python/heap_implementation.py
+++ Python/heap_implementation.py
@@ -22,6 +22,8 @@ class Heap:
         while idx > 1 and self.heap[idx // 2] > self.heap[idx]:
             self.heap[idx // 2], self.heap[idx] = self.heap[idx], self.heap[idx // 2]
             idx //= 2
+        
+        return
     
     def pop(self):
         if len(self.heap) == 1:
@@ -33,28 +35,60 @@ class Heap:
         result = self.heap[1]
         self.heap[1] = self.heap.pop()
         
-        idx = 1
-        while (2 * idx) < len(self.heap):  # this loop condition is check whether there's a left child
+        i = 1  # pointer of root node
+        while (2 * i) < len(self.heap):  # this loop condition is check whether there's a left child
             
             # this condition is check whether there's a right child
-            if ((2 * idx + 1) < len(self.heap) and
-                self.heap[2 * idx + 1] < self.heap[2 * idx] and  # check right child is less than left child
-                self.heap[1] > self.heap[2 * idx + 1]):  # check root node is greater than its right child
+            if ((2 * i + 1) < len(self.heap) and
+                self.heap[2 * i + 1] < self.heap[2 * i] and  # check right child is less than left child
+                self.heap[1] > self.heap[2 * i + 1]):  # check root node is greater than its right child
                 
                 # swap right child
-                self.heap[1], self.heap[2 * idx + 1] = self.heap[2 * idx + 1], self.heap[1]
+                self.heap[1], self.heap[2 * i + 1] = self.heap[2 * i + 1], self.heap[1]
                 
                 # update the pointer to right child
-                idx = 2 * idx + 1
+                i = 2 * i + 1
             
-            elif self.heap[1] > self.heap[2 * idx]:
+            elif self.heap[1] > self.heap[2 * i]:
                 # swap left child
-                self.heap[1], self.heap[2 * idx] = self.heap[2 * idx], self.heap[1]
+                self.heap[1], self.heap[2 * i] = self.heap[2 * i], self.heap[1]
                 
                 # update the pointer to left child
-                idx = 2 * idx
+                i = 2 * i
             
             else:
                 break
         
         return result
+    
+    def heapify(self, arr):
+        # move the first value into the last index
+        arr.append(arr[0])
+        self.heap = arr
+        
+        curr_idx = (len(self.heap) - 1) // 2  # pointer of the first non-leaf node
+        while curr_idx > 0:
+            i = curr_idx
+            
+            while (2 * i) < len(self.heap):
+                if ((2 * i + 1) < len(self.heap) and
+                    self.heap[2 * i + 1] < self.heap[2 * i] and
+                    self.heap[i] > self.heap[2 * i + 1]):
+                    
+                    # swap right child
+                    self.heap[i], self.heap[2 * i + 1] = self.heap[2 * i + 1], self.heap[i]
+                    
+                    # update the pointer to right child
+                    i = 2 * i + 1
+                
+                elif self.heap[i] > self.heap[2 * i]:
+                    # swap left child
+                    self.heap[i], self.heap[2 * i] = self.heap[2 * i], self.heap[i]
+                    
+                    # update pointer to left
+                    i = 2 * i
+                
+                else:
+                    break
+            
+            curr_idx -= 1
