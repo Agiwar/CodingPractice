92770ab07349e5938a79c7a6dc99e7945ab1f84a
Agiwar <agiwar791005@gmail.com>
2024-10-09 15:50:50 +0800

    add iteration method to implement fibonacci sequence

--- Python/fibonacci_sequence.py
+++ Python/fibonacci_sequence.py
@@ -12,19 +12,34 @@ def argparse_integer() -> argparse.Namespace:
     return args
 
 
-def fibonacci(n: int) -> int:
+def fibonacci_recursive(n: int) -> int:
     if n < 0:
         raise ValueError("Incorrect input.")
     elif n <= 1:
         return n
     else:
-        return fibonacci(n - 1) + fibonacci(n - 2)
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+    
+
+def fibonacci_iterative(n: int) -> int:
+    if n < 0:
+        raise ValueError("Incorrect input.")
+    
+    if n == 0:
+        return n
+    
+    a, b = 0, 1
+    for _ in range(1, n):
+        a, b = b, (a + b)
+    
+    return b
 
 
 if __name__ == "__main__":
     args = argparse_integer()
     n = args.int
-    n_th_fibonacci = fibonacci(n)
+    n_th_fibonacci_rec = fibonacci_recursive(n)
+    n_th_fibonacci_itr = fibonacci_iterative(n)
 
     # given n = 5, you may get 5; n = 10, get 55
-    print(n_th_fibonacci)
+    print(f"{n_th_fibonacci_rec} = {n_th_fibonacci_itr}")
