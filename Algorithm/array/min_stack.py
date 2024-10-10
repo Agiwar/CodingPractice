5cbb3986d7cefa10fc128729370c642a2fbf6075
Agiwar <agiwar791005@gmail.com>
2024-10-10 14:57:00 +0800

    make some changes

--- Algorithm/array/min_stack.py
+++ Algorithm/array/min_stack.py
@@ -1,26 +1,22 @@
 class MinStack:
 
     def __init__(self):
-        self.stack = []  # store the elements by "push method" order
-        self.min_stack = []  # store the min element by comparing current val and the last element in min_stack
-    
+        self.stack = []
+        self.min_stack = []
+
     def push(self, val: int) -> None:
         self.stack.append(val)
 
-        # check which number is more min: val or last element in min_stack
-        min_val = min(val, self.min_stack[-1] if self.min_stack else val)
+        if self.min_stack:
+            val = min(val, self.min_stack[-1])
+        self.min_stack.append(val)
 
-        # then push the more min number into min_stack
-        self.min_stack.append(min_val)
-        return None
-    
     def pop(self) -> None:
         self.stack.pop()
         self.min_stack.pop()
-        return None
-    
+
     def top(self) -> int:
         return self.stack[-1]
-    
-    def get_min(self) -> int:
+
+    def getMin(self) -> int:
         return self.min_stack[-1]
