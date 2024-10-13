b299f018003c382230c0976b8e5cac2ee2b6a669
Agiwar <agiwar791005@gmail.com>
2024-10-13 15:25:55 +0800

    another thought for solution

--- Python/bubble_sort.py
+++ Python/bubble_sort.py
@@ -1,7 +1,7 @@
 from typing import List
 
 
-def bubble_sort(nums: List[int]) -> List[int]:
+def bubble_sort_1(nums: List[int]) -> List[int]:
     n = len(nums) - 1
     
     if n <= 0:
@@ -13,6 +13,27 @@ def bubble_sort(nums: List[int]) -> List[int]:
             if nums[i] > nums[i + 1]:
                 need_to_swap = True
                 nums[i], nums[i + 1] = nums[i + 1], nums[i]
+        
+        n -= 1
+        
+        if not need_to_swap:
+            break
+    
+    return nums
+
+
+def bubble_sort_2(nums: List[int]) -> List[int]:
+    n = len(nums)
+    
+    if n <= 1:
+        return nums
+    
+    while n > 1:
+        need_to_swap = False
+        for i in range(1, len(nums)):
+            if nums[i - 1] > nums[i]:
+                need_to_swap = True
+                nums[i - 1], nums[i] = nums[i], nums[i - 1]
         
         n -= 1
         
