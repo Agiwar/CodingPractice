38052b687e29042a30bedc1cc155faac8b6585ab
Agiwar <agiwar791005@gmail.com>
2024-11-07 00:26:19 +0800

    add dynamic programming solutions

--- Algorithm/dinamic_programming/house_robber.py
+++ Algorithm/dinamic_programming/house_robber.py
@@ -2,7 +2,14 @@ from typing import List
 
 
 class Solution1:
+    # Use DFS to find all possibilities of maximum money robbed.
     def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        elif len(nums) == 2:
+            return max(nums[0], nums[1])
+        
         rob_money_each_house = {}
         
         
@@ -24,3 +31,44 @@ class Solution1:
         
         
         return max_total_money_robbed(0)
+
+
+class Solution2:
+    # Use dp array to store the maximum total money robbed up to the current house.
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        
+        elif len(nums) == 2:
+            return max(nums[0], nums[1])
+        
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        
+        for i in range(2, len(nums)):
+            rob_current_house = nums[i] + dp[i - 2]
+            skip_current_house = dp[i - 1]
+            
+            dp[i] = max(rob_current_house, skip_current_house)
+        
+        return dp[-1]
+
+
+class Solution3:
+    # Just use two variables to represent the maximum money robbed of current house 
+    # is determined by the previous two houses' results.
+    def rob(self, nums: List[int]) -> int:
+        num_of_house = len(nums)
+        
+        if num_of_house == 1:
+            return nums[0]
+        
+        elif num_of_house == 2:
+            return max(nums[0], nums[1])
+        
+        rob1, rob2 = nums[0], max(nums[0], nums[1])
+        for i in range(2, num_of_house):
+            rob1, rob2 = rob2, max(nums[i] + rob1, rob2)
+        
+        return rob2
