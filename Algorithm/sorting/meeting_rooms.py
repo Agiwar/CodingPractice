034017ead7a2bf7509202849e1f9c090584f3d20
Agiwar <agiwar791005@gmail.com>
2024-11-08 01:28:34 +0800

    use built-in sort

--- Algorithm/sorting/meeting_rooms.py
+++ Algorithm/sorting/meeting_rooms.py
@@ -1,8 +1,9 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     # Use bubble sort to sort all meetings by its start time.
+    # time = O(n^2)
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         n = len(intervals)
         
@@ -21,6 +22,19 @@ class Solution:
                 break
             
             n -= 1
+        
+        return (
+            all(intervals[i - 1][1] <= intervals[i][0] for i in range(1, len(intervals)))
+        )
+
+
+class Solution2:
+    # time = O(n * log n)
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if len(intervals) <= 1:
+            return True
+        
+        intervals.sort(key=lambda x: x[0])
         
         return (
             all(intervals[i - 1][1] <= intervals[i][0] for i in range(1, len(intervals)))
