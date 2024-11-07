bd3942742fab18c50812b7685d59d1842510cb0d
Agiwar <agiwar791005@gmail.com>
2024-11-08 01:38:05 +0800

    no sorting

--- Algorithm/sorting/meeting_rooms.py
+++ Algorithm/sorting/meeting_rooms.py
@@ -39,3 +39,21 @@ class Solution2:
         return (
             all(intervals[i - 1][1] <= intervals[i][0] for i in range(1, len(intervals)))
         )
+
+
+class Solution3:
+    # Use set() to store the meeting time which is occupied.
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        if len(intervals) <= 1:
+            return True
+        
+        time_occupied = set()
+        for meeting in intervals:
+            for meeting_time in range(meeting[0], meeting[1]):
+                if meeting_time not in time_occupied:
+                    time_occupied.add(meeting_time)
+                
+                else:
+                    return False  # The meeting time has been occupied.
+        
+        return True
