65a3a7e68c9649698f76d85f0b8b0397e1ff9ddb
Agiwar <agiwar791005@gmail.com>
2024-11-17 23:40:55 +0800

    add maximum_subarray.py file

---
+++ Algorithm/array/maximum_subarray.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = 0
+        max_sum = nums[0]
+
+        for num in nums:
+            current_sum = max(current_sum, 0)
+            current_sum += num
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
