943792baa38ca79ee548f2a368113a5d508b1e99
Agiwar <agiwar791005@gmail.com>
2024-11-17 19:13:52 +0800

    clean code solution

--- Algorithm/string/longest_palindromic_substring.py
+++ Algorithm/string/longest_palindromic_substring.py
@@ -1,4 +1,4 @@
-class Solution:
+class Solution1:
     def longestPalindrome(self, s: str) -> str:
         if len(s) == 1:
             return s
@@ -13,7 +13,7 @@ class Solution:
             # when pointers are inbound, and char are the same (cuz need to be palindromic string)
             while L >= 0 and R < len(s) and s[L] == s[R]:
 
-                # check this sug string length
+                # check this sub string length
                 if (R - L + 1) > res_len:
                     res = s[L:R+1]
                     res_len = R - L + 1
@@ -21,7 +21,7 @@ class Solution:
                 L -= 1
                 R += 1
             
-            # even length: same code with odd lenth part, just control the pointer
+            # even length: same code with odd length part, just control the pointer
             L, R = i, i + 1
             while L >= 0 and R < len(s) and s[L] == s[R]:
                 if (R - L + 1) > res_len:
@@ -32,3 +32,45 @@ class Solution:
                 R += 1
 
         return res
+
+
+class Solution2:
+    def find_out_longest_palindromic_substring(
+        self,
+        s: str,
+        left_pointer: int,
+        right_pointer: int
+    ) -> str:
+        sub_string = ""
+        max_sub_string_length = 0
+        
+        while (
+            left_pointer >= 0 and 
+            right_pointer < len(s) and
+            s[left_pointer] == s[right_pointer]
+        ):
+            if (right_pointer - left_pointer + 1) >  max_sub_string_length:
+                sub_string = s[left_pointer:(right_pointer + 1)]
+                max_sub_string_length = len(sub_string)
+            
+            left_pointer -= 1
+            right_pointer += 1
+        
+        return sub_string
+
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 1:
+            return s
+
+        max_palindromic = ""
+        for idx in range(len(s)):
+            odd_palindromic = self.find_out_longest_palindromic_substring(s, idx, idx)
+            even_palindromic = self.find_out_longest_palindromic_substring(s, idx, idx + 1)
+            max_palindromic = max(
+                max_palindromic,
+                odd_palindromic,
+                even_palindromic,
+                key=len
+            )
+        
+        return max_palindromic
