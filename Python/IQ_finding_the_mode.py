8c47ea08945687527c6b391d7648b506f53d7512
Agiwar <agiwar791005@gmail.com>
2024-11-22 23:27:31 +0800

    clean code

--- Python/IQ_finding_the_mode.py
+++ Python/IQ_finding_the_mode.py
@@ -2,7 +2,7 @@ from collections import Counter
 from typing import List
 
 
-def mode(nums: List[int]) -> List[int]:
+def mode1(nums: List[int]) -> List[int]:
     nums_occur = dict(sorted(Counter(nums).items()))
     max_num_occur = max(nums_occur.values())
 
@@ -12,3 +12,12 @@ def mode(nums: List[int]) -> List[int]:
             res.append(num)
     
     return res
+
+
+def mode1(nums: List[int]) -> List[int]:
+    if not nums: return []
+    
+    num_occur = Counter(nums)
+    max_num_occur = max(num_occur.values())
+    
+    return sorted(num for num, occur in num_occur.items() if occur == max_num_occur)
