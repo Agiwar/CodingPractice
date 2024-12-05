1bced412be09ddc8e21aaa7626e709aad4769ea0
Agiwar <agiwar791005@gmail.com>
2024-12-05 21:54:57 +0800

    make some changes

--- Python/IQ_counting_file_lines.py
+++ Python/IQ_counting_file_lines.py
@@ -3,14 +3,12 @@
 # How would you count the total number of lines in the file in Python?
 
 
-CHUNK_SIZE = 8192 * 1024
-
-
-def count_file_lines(file_path: str) -> int:
+def count_file_lines(file_path: str, chunk_size: int = 8192 * 1024) -> int:
+    # 8192 * 1024 = 8MB
     line_ct = 0
     with open(file_path, "r") as f:
         while True:
-            if buffer := f.read(size=CHUNK_SIZE):
+            if buffer := f.read(size=chunk_size):
                 line_ct += buffer.count("\n")
             else:
                 break
