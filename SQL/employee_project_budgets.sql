3a652ef244db84340b9b772d8206e52e193b6a7d
Agiwar <agiwar791005@gmail.com>
2024-12-07 00:00:03 +0800

    make some changes

--- SQL/employee_project_budgets.sql
+++ SQL/employee_project_budgets.sql
@@ -1,13 +1,24 @@
+WITH deduplicate_employee_projects AS (  -- use GROUP BY to deduplicate instead of DISTINCT
+    SELECT
+        project_id,
+        employee_id
+    FROM
+        employee_projects
+    GROUP BY
+        project_id,
+        employee_id
+)
 SELECT
-    p.title AS title,
-    p.budget / COUNT(e.employee_id) AS budget_per_employee
+    p.title,
+    p.budget::NUMERIC / COUNT(d.employee_id)::NUMERIC AS budget_per_employee
 FROM
     projects AS p
 INNER JOIN
-    employee_projects AS e
-    ON p.id = e.project_id
-GROUP BY
-    p.id
+    deduplicate_employee_projects AS d
+    ON p.id = d.project_id
+GROUP BY                                -- each project by the budget
+    p.title,
+    p.budget
 ORDER BY
     budget_per_employee DESC
-LIMIT 5
+LIMIT 5;
