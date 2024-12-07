1e995f26053cbce413637223ab568e5541ba89c0
Agiwar <agiwar791005@gmail.com>
2024-12-07 15:00:43 +0800

    make some changes

--- SQL/employee_salaries.sql
+++ SQL/employee_salaries.sql
@@ -1,20 +1,16 @@
-WITH big_department AS (
-    SELECT
-        AVG(CASE WHEN e.salary > 100000 THEN 1 ELSE 0 END) AS percentage_over_100k,
-        d.name AS department_name,
-        COUNT(e.id) AS number_of_employees
-    FROM
-        employees AS e
-    INNER JOIN
-        departments AS d
-        ON e.department_id = d.id
-    GROUP BY
-        e.department_id
-    HAVING
-        COUNT(e.id) >= 10
-    ORDER BY
-        number_of_employees DESC
-    LIMIT 3
-)
-
-SELECT * FROM big_department ORDER BY percentage_over_100k
+SELECT
+    AVG(CASE WHEN e.salary > 100000 THEN 1 ELSE 0 END) AS percentage_over_100k,
+    d.name AS department_name,
+    COUNT(e.id) AS number_of_employees
+FROM
+    employees AS e
+INNER JOIN
+    departments AS d
+    ON e.department_id = d.id
+GROUP BY
+    d.name
+HAVING
+    COUNT(e.id) >= 10
+ORDER BY
+    percentage_over_100k DESC
+LIMIT 3;
