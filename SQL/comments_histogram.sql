04a0ab0326fa1895ebb89e7d9f6b58d53069ced5
Agiwar <agiwar791005@gmail.com>
2024-12-07 15:45:08 +0800

    make some changes

--- SQL/comments_histogram.sql
+++ SQL/comments_histogram.sql
@@ -1,21 +1,20 @@
-WITH calculate_comment_count AS (
+-- directly filter out body is null and the comment date is not 2020-01
+WITH user_has_comment AS (
     SELECT
-        u.id AS id
-        , SUM(CASE WHEN c.body IS NULL THEN 0 ELSE 1 END) AS comment_count
+        u.id AS user_id,
+        SUM(CASE WHEN c.body IS NULL OR TO_CHAR(c.created_at, 'YYYY-MM') != '2020-01' THEN 0 ELSE 1 END) AS comment_count 
     FROM
         users AS u
     LEFT JOIN
         comments AS c
         ON u.id = c.user_id
-        AND TO_CHAR(c.created_at, 'YYYY-MM') = '2020-01'
     GROUP BY
         u.id
 )
-
 SELECT
-    comment_count
-    , COUNT(id) AS frequency
+    comment_count,
+    COUNT(user_id) AS frequency
 FROM
-    calculate_comment_count
+    user_has_comment
 GROUP BY
     comment_count
