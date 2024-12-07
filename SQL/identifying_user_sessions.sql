38d89d6c75ee3317f4a3dea0bb557247b3066e7e
Agiwar <agiwar791005@gmail.com>
2024-12-07 14:28:55 +0800

    make some changes

--- SQL/identifying_user_sessions.sql
+++ SQL/identifying_user_sessions.sql
@@ -1,31 +1,41 @@
-WITH calculate_event_minute_diff AS (
+-- calculate the minute difference between events by each user
+WITH each_user_event_min_diff AS (
     SELECT
-        created_at
-        , user_id
-        , event
-        , TIMESTAMPDIFF(
-            MINUTE
-            , LAG(created_at) OVER(PARTITION BY user_id ORDER BY created_at)
-            , created_at
-        ) AS minute_diff
+        created_at,
+        user_id,
+        event,
+        TIMESTAMPDIFF(MINUTE, LAG(created_at, 1, created_at) OVER(PARTITION BY user_id ORDER BY created_at), created_at) AS min_diff
     FROM
         events
-)
-
-, check_is_new_session AS (
+),
+-- calculate teh cumulative minute difference by each user
+cumulative_min_diff AS (
     SELECT
-        created_at
-        , user_id
-        , event
-        , CASE WHEN minute_diff > 60 OR minute_diff IS NULL THEN 1 ELSE 0 END AS is_new_session
+        created_at,
+        user_id,
+        event,
+        SUM(min_diff) OVER(PARTITION BY user_id ORDER BY created_at) AS cum_min_diff
     FROM
-        calculate_event_minute_diff
+        each_user_event_min_diff
+),
+-- define the session time frame, i.e., 0~60, 61~120, 121~180, etc
+group_same_session AS (
+    SELECT
+        created_at,
+        user_id,
+        event,
+        CASE
+            WHEN cum_min_diff % 60 = 0 AND cum_min_diff > 0 THEN FLOOR((cum_min_diff - 1) / 60) + 1
+            ELSE FLOOR(cum_min_diff / 60) + 1
+        END AS grp
+    FROM
+        cumulative_min_diff
 )
-
+-- use dense rank to assign the session id for each user if the events are in the same time frame
 SELECT
-    created_at
-    , user_id
-    , event
-    , SUM(is_new_session) OVER(PARTITION BY user_id ORDER BY created_at) AS session_id
+    created_at,
+    user_id,
+    event,
+    DENSE_RANK() OVER(PARTITION BY user_id ORDER BY grp) AS session_id
 FROM
-    check_is_new_session
+    group_same_session
