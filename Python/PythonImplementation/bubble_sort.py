012b68b1197757c3fe1b5f7e4d790b87e246525e
Agiwar <agiwar791005@gmail.com>
2024-12-09 16:27:22 +0800

    remove unnecessary code

--- Python/PythonImplementation/bubble_sort.py
+++ Python/PythonImplementation/bubble_sort.py
@@ -1,28 +1,7 @@
 from typing import List
 
 
-def bubble_sort_1(nums: List[int]) -> List[int]:
-    n = len(nums) - 1
-    
-    if n <= 0:
-        return nums
-    
-    while n > 0:
-        need_to_swap = False
-        for i in range(n):
-            if nums[i] > nums[i + 1]:
-                need_to_swap = True
-                nums[i], nums[i + 1] = nums[i + 1], nums[i]
-
-        if not need_to_swap:
-            break
-        
-        n -= 1
-    
-    return nums
-
-
-def bubble_sort_2(nums: List[int]) -> List[int]:
+def bubble_sort_(nums: List[int]) -> List[int]:
     n = len(nums)
     
     if n <= 1:
