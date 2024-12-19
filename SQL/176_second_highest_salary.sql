85328979d257872835191dd24cb1a4a212aee953
Agiwar <agiwar791005@gmail.com>
2024-12-19 21:19:13 +0800

    different approaches and do time complexity analysis

--- SQL/176_second_highest_salary.sql
+++ SQL/176_second_highest_salary.sql
@@ -1,5 +1,19 @@
--- solution 1: dense_rank + cte
-WITH cte AS (
+-- This difficulty of this question is if there's no second highest salary, need to return NULL not empty record.
+
+
+-- solution 1: intuitive but not so efficient due to NOT IN syntax
+-- time = O(n) + O(n) ~= O(n)
+SELECT
+    MAX(salary) AS SecondHighestSalary
+FROM
+    Employee
+WHERE
+    salary NOT IN (SELECT MAX(salary) FROM Employee);
+
+
+-- solution 2: dense_rank
+-- time = O(n * log n) + O(n) ~= O(n * log n)
+WITH ranking_salary AS (
     SELECT
         salary,
         DENSE_RANK() OVER(ORDER BY salary DESC) AS r
@@ -9,22 +23,39 @@ WITH cte AS (
 SELECT 
     max(salary) AS SecondHighestSalary
 FROM
-    cte
+    ranking_salary
 WHERE
-    r > 1
+    r = 2;
 
 
--- solution 2: limit + offset
-WITH cte AS (
-    SELECT DISTINCT
+-- solution 3: limit + offset
+-- time = O(n * log n) + O(n) ~= O(n * log n)
+SELECT (
+    SELECT
         salary
     FROM
         Employee
-    ORDER BY salary DESC
+    GROUP BY
+        salary
+    ORDER BY
+        salary DESC
     LIMIT 1
     OFFSET 1
-)
+) AS SecondHighestSalary;
+
+
+-- solution 4: limit + offset + case when
+-- time = O(n) + O(n * log n) ~= O(n * log n)
 SELECT
-    max(salary) AS SecondHighestSalary
-FROM
-    cte
+    CASE WHEN COUNT(*) = 1 THEN NULL ELSE MIN(salary) AS SecondHighestSalary
+FROM (
+    SELECT
+        salary
+    FROM
+        Employee
+    GROUP BY
+        salary
+    ORDER BY
+        salary DESC
+    LIMIT 2
+) AS t;
