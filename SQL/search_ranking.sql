4df99c5f0df3b725699bc9b06f18938b65eb4aa5
Agiwar <agiwar791005@gmail.com>
2024-12-21 01:11:26 +0800

    IQ: Search Ranking

---
+++ SQL/search_ranking.sql
@@ -0,0 +1,37 @@
+-- You’re given a table that represents search results from searches on Facebook.
+-- The query column is the search term, position column represents each position the search result came in,
+-- and the rating column represents the human rating of the result from 1 to 5 where 5 is high relevance and 1 is low relevance.
+
+-- Write a query to get the percentage of search queries where all of the ratings for the query results are less than a rating of 3.
+-- Please round your answer to two decimal points.
+
+-- search_results table
+
+-- result_id	INTEGER (primary key)
+-- query	    TEXT
+-- position	    INTEGER
+-- rating	    INTEGER
+
+-- Output:
+-- percentage_less_than_3	FLOAT
+
+
+
+-- solution: find out each query's all ratings are less than three
+
+WITH ratings_each_query AS (
+    SELECT
+        query,
+        COUNT(ratings) AS total_ratings,
+        SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END) AS total_less_than_3_ratings
+    FROM
+        search_results
+    GROUP BY
+        query
+)
+SELECT
+    ROUND(COUNT(query)::NUMERIC / (SELECT COUNT(query) FROM ratings_each_query)::NUMERIC, 2) AS percentage_less_than_3
+From
+    ratings_each_query
+WHERE
+    total_ratings = total_less_than_3_ratings;
