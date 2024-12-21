67a5e779e5f17244e3e2ae2269ccf85bc4258075
Agiwar <agiwar791005@gmail.com>
2024-12-21 17:21:10 +0800

    modify session time unit to second

--- SQL/identifying_user_sessions.sql
+++ SQL/identifying_user_sessions.sql
@@ -1,41 +1,41 @@
--- calculate the minute difference between events by each user
-WITH each_user_event_min_diff AS (
+-- calculate the second difference between events by each user
+WITH event_time_diff_each_user AS (
     SELECT
         created_at,
         user_id,
         event,
-        TIMESTAMPDIFF(MINUTE, LAG(created_at, 1, created_at) OVER(PARTITION BY user_id ORDER BY created_at), created_at) AS min_diff
+        TIMESTAMPDIFF(SECOND, LAG(created_at, 1, created_at) OVER(PARTITION BY user_id ORDER BY created_at), created_at) AS event_sec_diff
     FROM
         events
 ),
--- calculate the cumulative minute difference by each user
-cumulative_min_diff AS (
+-- calculate the cumulative second difference by each user
+cumulate_event_time_diff AS (
     SELECT
         created_at,
         user_id,
         event,
-        SUM(min_diff) OVER(PARTITION BY user_id ORDER BY created_at) AS cum_min_diff
+        SUM(event_sec_diff) OVER(PARTITION BY user_id ORDER BY created_at) AS event_sec_diff_cum
     FROM
-        each_user_event_min_diff
+        event_time_diff_each_user
 ),
--- define the session time frame, e.g., 0~60, 61~120, 121~180, etc
-group_same_session AS (
+-- define the session time frame, e.g., 0~3600, 3601~7200, 7201~10800, etc
+grouping_session_time_window AS (
     SELECT
         created_at,
         user_id,
         event,
         CASE
-            WHEN cum_min_diff % 60 = 0 AND cum_min_diff > 0 THEN FLOOR((cum_min_diff - 1) / 60) + 1
-            ELSE FLOOR(cum_min_diff / 60) + 1
-        END AS grp
+            WHEN event_sec_diff_cum > 0 AND event_sec_diff_cum % 3600 = 0 THEN FLOOR((event_sec_diff_cum - 1) / 3600) + 1
+            ELSE FLOOR(event_sec_diff_cum / 3600) + 1
+        END AS session_time_window
     FROM
-        cumulative_min_diff
+        cumulate_event_time_diff
 )
 -- use dense rank to assign the session id for each user if the events are in the same time frame
 SELECT
     created_at,
     user_id,
     event,
-    DENSE_RANK() OVER(PARTITION BY user_id ORDER BY grp) AS session_id
+    DENSE_RANK() OVER(PARTITION BY user_id ORDER BY session_time_window) AS session_id
 FROM
-    group_same_session
+    grouping_session_time_window
