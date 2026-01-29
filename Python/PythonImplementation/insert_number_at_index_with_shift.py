1ce122e500cb66eff5670b0be34fcd1fd7783277
Jeffrey <agiwar791005@gmail.com>
2026-01-29 16:45:04 +0800

    python: implement insert number at index with shift

---
+++ Python/PythonImplementation/insert_number_at_index_with_shift.py
@@ -0,0 +1,53 @@
+def insert_num_fixed_arr(
+    arr: list[int] | None,
+    num: int | None,
+    i: int,
+) -> list[int]:
+
+    if not arr or num is None:
+        return arr or []
+
+    n = len(arr)
+    if not (-n <= i < n):
+        raise IndexError("Out of index.")
+
+    elif 0 < i < n - 1:
+        arr[:i] = arr[1:i + 1]
+
+    elif -n < i < -1:
+        arr[i + 1:] = arr[i:-1]
+
+    arr[i] = num
+    return arr
+
+
+
+def test_insert_num_fixed_arr():  # sourcery skip: use-contextlib-suppress
+    num = 5
+    assert insert_num_fixed_arr([1, -4, 2, 3], num, 0) == [5, -4, 2, 3]
+    assert insert_num_fixed_arr([1, -4, 2, 3], num, 1) == [-4, 5, 2, 3]
+    assert insert_num_fixed_arr([1, -4, 2, 3], num, 2) == [-4, 2, 5, 3]
+    assert insert_num_fixed_arr([1, -4, 2, 3], num, 3) == [1, -4, 2, 5]
+    assert insert_num_fixed_arr([1, -4, 2, 3], num, -1) == [1, -4, 2, 5]
+    assert insert_num_fixed_arr([1, -4, 2, 3], num, -2) == [1, -4, 5, 2]
+    assert insert_num_fixed_arr([1, -4, 2, 3], num, -3) == [1, 5, -4, 2]
+    assert insert_num_fixed_arr([1, -4, 2, 3], num, -4) == [5, -4, 2, 3]
+    assert insert_num_fixed_arr([], num, 1) == []
+    assert insert_num_fixed_arr([1, -4, 2, 3], None, 1) == [1, -4, 2, 3]
+    assert insert_num_fixed_arr([1], num, 0) == [num]
+
+    try:
+        insert_num_fixed_arr([1, -4, 2, 3], num, 4)
+        assert False, "Out of index."
+    except IndexError:
+        pass
+
+    try:
+        insert_num_fixed_arr([1, -4, 2, 3], num, -5)
+        assert False, "Out of index."
+    except IndexError:
+        pass
+
+
+if __name__ == "__main__":
+    test_insert_num_fixed_arr()
