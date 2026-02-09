83010b4dc42a88718132208c176755e6d43cafba
Jeffrey <agiwar791005@gmail.com>
2026-02-09 15:57:35 +0800

    https://leetcode.com/problems/move-zeroes/

--- LeetCode/DSA/array/move_zeroes.py
+++ LeetCode/DSA/array/move_zeroes.py
@@ -5,15 +5,28 @@ class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
+        The input nums is not sorted, but the requirement is simple,
+            just make all zero-value numbers come after non-zero-value numbers
+            but need to keep the original relative order.
+        
+        Define writer (idx_w) and reader (idx_r), respectively.
+            idx_w handles checking the current position's number is zero or not
+            idx_r goes through entire array until we find out non-zero
+            which can replace that current zero-value number
+        
+        time = O(n)
+        space = (1), in-place manipulation
         """
+
+        idx_w =0
+        for idx_r in range(len(nums)):
+            if nums[idx_r] != 0:
+                nums[idx_w] = nums[idx_r]
+                idx_w += 1
         
-        k = 0
-        for idx in range(len(nums)):
-            if nums[idx] != 0:
-                nums[k] = nums[idx]
-                k += 1
+        nums[idx_w:] = [0] * (len(nums) - idx_w)
         
-        nums[k:] = [0] * (len(nums) - k)
 
 
 moveZeroes = Solution().moveZeroes
