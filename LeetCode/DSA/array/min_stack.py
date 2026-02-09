3645ebd1a8f8d612d289cccc2818d1d9f151eb65
Jeffrey <agiwar791005@gmail.com>
2026-02-09 15:57:35 +0800

    https://leetcode.com/problems/min-stack/

--- LeetCode/DSA/array/min_stack.py
+++ LeetCode/DSA/array/min_stack.py
@@ -1,14 +1,27 @@
 class MinStack:
     """
-    You must implement a solution with O(1) time complexity for each function.
+    Implement a stack that has method getMin has O(1) time complexity,
+        so it's not allowed to go through the entire stack to find out the minium value.
     
-    time = O(1)
-    space = 2 * O(n) ~ O(n)
+    The main idea behind the code is, implement 2 stack which are stack and min_stack,
+        stack holds the normal stack property, cuz push, pop, and top is already O(1) time
+        but min_stack is the key which this min_stack just stores the current pushed number's value
+        is equal or less than the min_stack last number value (the append min value last time),
+        we don't need to store each pushed number,
+        and then compare the current one and min_stack's last one either
+        just tracking the current pushed value is what we want.
+    
+    Note that when pop stack, if this number is equal to min stack last number,
+        min stack does pop as well, which means stack pop the minium value from stack
+        so the number of minium in stack has been extracted by 1
+    
+    time = O(1) for all methods
+    space = O(n) for two stack created
     """
-
-    def __init__(self) -> None:
+    
+    def __init__(self):
         self.stack = []
-        self._min_stack = []
+        self._min_stack = []        
 
     def push(self, val: int) -> None:
         self.stack.append(val)
