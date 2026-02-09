75d06c3a166e6dc783b58bb1775933dd2e0632b9
Jeffrey <agiwar791005@gmail.com>
2026-02-09 15:57:36 +0800

    https://leetcode.com/problems/remove-element/

--- LeetCode/DSA/array/remove_element.py
+++ LeetCode/DSA/array/remove_element.py
@@ -4,19 +4,33 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         """
+        Input is unsorted, but it doesn't matter,
+            we only wanna remove the specific number from array
+        
+        If nums.length has min value of zero, so directly return 0 anyway
+        Think about what if nums.length is one, and the number is equal to val,
+            which means this number must be removed from nums, so return zero as well
+        
+        Based on these situations above, we need to check whether or not
+            the first number with zero index is needed to be removed.
+        
+        Define a writer (idx_w) and reader (idx_r) pointer, respectively
+            idx_w handles the current position's number should be re-written or not,
+            idx_r is a pointer to just go through the entire array,
+            checking the current number can be a re-written number for those idx_w
+        
         time = O(n)
-        space = O(1)
+        space = O(1), in-place manipulation
         """
-        if not nums:
-            return 0
+
+        idx_w = 0
+        for idx_r in range(len(nums)):
+            if nums[idx_r] != val:
+                nums[idx_w] = nums[idx_r]
+                idx_w += 1
         
-        k = 0
-        for idx in range(len(nums)):
-            if nums[idx] != val:
-                nums[k] = nums[idx]
-                k += 1
-        
-        return k
+        return idx_w
+
 
 
 removeElement = Solution().removeElement
