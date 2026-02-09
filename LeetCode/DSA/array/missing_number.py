665e88107f55ba5920a7beae3ccb52b3b281d1fc
Jeffrey <agiwar791005@gmail.com>
2026-02-09 15:57:35 +0800

    https://leetcode.com/problems/missing-number/

--- LeetCode/DSA/array/missing_number.py
+++ LeetCode/DSA/array/missing_number.py
@@ -1,15 +1,20 @@
 from typing import List
 
-
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        # Given an array nums containing n distinct numbers in the range [0, n]  << included both sides
-        # but there's a missing number in nums, find out it.
+        pass
 
-        expected_sum = sum(range(len(nums) + 1))  # time = O(n)
-        actual_sum = sum(nums)  # time = O(n)
-        
-        # overall time = O(n)
-        # space = O(1)
+missingNumber = Solution().missingNumber
 
-        return expected_sum - actual_sum
+def test_missingNumber():
+    # LeetCode examples
+    assert missingNumber([3,0,1]) == 2
+    assert missingNumber([0,1]) == 2
+    assert missingNumber([9,6,4,2,3,5,7,0,1]) == 8
+
+    # Edge cases (write your own)
+
+    print("All tests passed")
+
+if __name__ == "__main__":
+    test_missingNumber()
