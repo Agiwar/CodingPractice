59f082713735e8d19cfad5c1766fcd835b1d6a4e
Jeffrey <agiwar791005@gmail.com>
2026-02-09 16:04:06 +0800

    Revert "https://leetcode.com/problems/maximum-subarray/"
    This reverts commit 57ad3e3be0c63d10bae7acafaf67292bf8b4d657.

--- LeetCode/DSA/array/maximum_subarray.py
+++ LeetCode/DSA/array/maximum_subarray.py
@@ -1,20 +1,14 @@
 from typing import List
 
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        pass
+        current_sum = 0
+        max_sum = nums[0]
 
-maxSubArray = Solution().maxSubArray
+        for num in nums:
+            current_sum = max(current_sum, 0)
+            current_sum += num
+            max_sum = max(max_sum, current_sum)
 
-def test_maxSubArray():
-    # LeetCode examples
-    assert maxSubArray([-2,1,-3,4,-1,2,1,-5,4]) == 6
-    assert maxSubArray([1]) == 1
-    assert maxSubArray([5,4,-1,7,8]) == 23
-
-    # Edge cases (write your own)
-
-    print("All tests passed")
-
-if __name__ == "__main__":
-    test_maxSubArray()
+        return max_sum
