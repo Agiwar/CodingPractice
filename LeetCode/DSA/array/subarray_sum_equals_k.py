7e42220377e3b9ee49b0655bdd5063b8ff217f32
Jeffrey <agiwar791005@gmail.com>
2026-02-10 20:20:33 +0800

    https://leetcode.com/problems/subarray-sum-equals-k/

--- LeetCode/DSA/array/subarray_sum_equals_k.py
+++ LeetCode/DSA/array/subarray_sum_equals_k.py
@@ -1,43 +1,69 @@
 from typing import List
 
-
-class Solution1:
-    # brute force
+class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        total_aub_arr_ct = 0
-        
-        for i in range(len(nums)):
-            curr_sum = 0
-            
-            for j in range(i, len(nums)):
-                curr_sum += nums[j]
-                
-                if curr_sum == k:
-                    total_aub_arr_ct += 1
-        
-        return total_aub_arr_ct
+        """
+        The input array is not ordered and may contain negative numbers.
+            When traversing the nums, calculate the current sum is equal to target k number,
+            if yes, we get a solution, and keep going through nums, there may be another solution,
+            and the previous solution, say prev_subarray, combines the current solution, say curr_subarray
+            will become a solution as well, i.e., prev_subarray + curr_subarray = new_subarray, it's also a solution
+            so we have prev_subarray, curr_subarray, and new_subarray which has 3 solutions
 
+        For the brute force, we use two pointer to track any possible subarray
+            which has summation value of target value k, this has O(n^2) time
+    
+        Above logic we can store the previous solution in a hashmap,
+            so no need to re-check the same portion while traversing
 
-class Solution2:
-    # use a hash map to store the cumulative we've already done,
-    # to avoid re-calculating the sum of sub array inside j loop when iterating i loop
-    # the curr sum of sub array can be resented as sum(arr[:b]) = sum(arr[:a]) + sum(arr[a:b])
-    # if there's a sub array arr[a:b] whose sum is target number k,
-    # so the previous cumulative sum of sub array sum(arr[:a]) = sum(arr[:b]) - sum(arr[a:b])
-    # which is sum(arr[:a]) = curr_sum - k, and use a hash map to store the occurrence of sum(arr[:a])
-    def subarraySum(self, nums: List[int], k: int) -> int:
-        # there must be the key cumulative sum = 0 at the beginning 
-        prev_cumulative_sum_occur = {0: 1}
+        The main idea behind the code is calculate the current sum of subarray,
+            and check its previous sum is in previous solution hashmap or not,
+            if yes, we get this solution plus the current subarray solution,
+            which means the current subarray can remove somewhat subarray to keep it still as solution
+
+        The base case is that when seeing the first num from nums, it has no prefix sum,
+            and the edge case is that this nums has length of 1
+
+        time = O(n)
+        space = O(1)
+        """
+        
+        if len(nums) == 1:
+            return 1 if nums[0] == k else 0
         
         curr_sum = 0
-        total_aub_arr_ct = 0
+        res = 0
+        prefix_sums = {0: 1}
         
         for num in nums:
             curr_sum += num
+            diff = curr_sum - k
             
-            if (prev_cumulative_sum := curr_sum - k) in prev_cumulative_sum_occur:
-                total_aub_arr_ct += prev_cumulative_sum_occur[prev_cumulative_sum]
-                
-            prev_cumulative_sum_occur[curr_sum] = prev_cumulative_sum_occur.get(curr_sum, 0) + 1
+            res += prefix_sums.get(diff, 0)
+            prefix_sums[curr_sum] = prefix_sums.get(curr_sum, 0) + 1
         
-        return total_aub_arr_ct
+        return res
+
+
+
+subarraySum = Solution().subarraySum
+
+def test_subarraySum():
+    assert subarraySum([1,1,1], 2) == 2
+    assert subarraySum([1,2,3], 3) == 2
+
+    # Edge cases
+    assert subarraySum([5], 1) == 0
+    assert subarraySum([5], 5) == 1
+    assert subarraySum([1, 2, 3], k = 3) == 2
+    assert subarraySum([3, 1, 2], k = 3) == 2
+    assert subarraySum([2, 3, 1], k = 3) == 1
+    assert subarraySum([-1, -1, -1], -1) == 3
+    assert subarraySum([-2, -1, 0, 1, 2], 0) == 3
+    assert subarraySum([-2, 2, 0, 1, -1], 0) == 6
+
+
+    print("All tests passed")
+
+if __name__ == "__main__":
+    test_subarraySum()
