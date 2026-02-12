74e3ca907348e27789c55643653bf6f93af5cb97
Jeffrey <agiwar791005@gmail.com>
2026-02-12 18:26:01 +0800

    https://leetcode.com/problems/valid-anagram/

---
+++ LeetCode/DSA/hashing/valid_anagram.py
@@ -0,0 +1,44 @@
+from typing import List
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        """
+        s and t must have a minimum length of 1, so if both of them are 1, s[0] != t[0], then False
+            and obviously, if s.length is not equal to t.length, return False directly
+
+        The main idea behind my code is calculate the occurrence of each char for s and t,
+            if the different type of char of occurrence in s and t is the same,
+            it means t can be re-arranged in somewhat way to be s
+
+        s = "anagram", t = "nagaram" -> True
+        s = "rat", t = "car" -> False
+        
+        using Counter:
+        time = O(n)
+        space = O(k), k is number of unique char, but can say O(1),
+            cuz letter only contains lowercase english
+        """
+
+        from collections import Counter
+        return Counter(s) == Counter(t)
+
+
+
+isAnagram = Solution().isAnagram
+
+def test_isAnagram():
+    assert isAnagram("anagram", "nagaram") == True
+    assert isAnagram("rat", "car") == False
+
+    # Edge cases
+    assert isAnagram("a", "b") == False
+    assert isAnagram("ab", "b") == False
+    assert isAnagram("aab", "abb") == False
+    assert isAnagram("google", "elgoog") == True
+    assert isAnagram("xxx", "xxx") == True
+
+
+    print("All tests passed")
+
+if __name__ == "__main__":
+    test_isAnagram()
