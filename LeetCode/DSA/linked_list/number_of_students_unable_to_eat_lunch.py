f4af827357866e8922fe67138b7eb7e6a55e487c
Jeffrey <agiwar791005@gmail.com>
2026-03-20 19:35:35 +0800

    https://leetcode.com/problems/number-of-students-unable-to-eat-lunch/

--- LeetCode/DSA/linked_list/number_of_students_unable_to_eat_lunch.py
+++ LeetCode/DSA/linked_list/number_of_students_unable_to_eat_lunch.py
@@ -3,17 +3,25 @@ from typing import List
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         """
-        the main idea behind the code is doing while looping to check the first student is fit first sandwich or not,
-            the relevant order of sandwiches is immutable (exclude the first sandwich consumed)
-            if the student doesn't have their favorite sandwich, pop this student and append it in last position
-            if student has sandwich, continue consuming until they don't
-            pop first student and append student both takes O(1) time,
-            but making sure all students they've seen the first sandwich, needs to traversing students
-            edge case is students.length is one, if the only one student doesn't have their preferred sandwich,
-            return 1 directly, on the other hand, if students and sandwiches are identical, return 0 directly
+        the main idea behind the code is determine what student's preference is,
+            how many "0" type students want "0" type sandwiches, and so on "1" type
 
-        time = O(n^2), n is student.length
-        space = O(1)
+            edge case is students.length is one,
+            if the only one student doesn't have their preferred sandwich, return 1 directly,
+            on the other hand, if students and sandwiches are identical, return 0 directly.
+
+        cuz the students queue will continue looping until that student gets their sandwich,
+            it's like a circular looping for students, so the order of students doesn't matter,
+            sandwiches' order is immutable, but that's not important,
+            calculate how many *1* type sandwich, and *0*'s,
+            traversing the sandwiches categories, if the current can be taken in students whoever,
+            subtract total number of students by 1, and so on,
+            if traversal done, means everyone is happy,
+            but if the current sandwich can't be taken from someone, exit looping directly,
+            then the current number of students is answer
+
+        time = O(m), m is sandwiches.length
+        space = O(1), stud_types counter always has length 2 regardless of students.length
         """
         
         if len(students) == 1 and students[0] != sandwiches[0]:
@@ -22,28 +30,19 @@ class Solution:
         elif students == sandwiches:
             return 0
         
-        idx = 0
-        ct_students = len(students)
-        seen = 0
         
-        while idx < ct_students:
-            sandwich = sandwiches[idx]
-            
-            if students[idx] != sandwich:
-                seen += 1
-                
-                if seen == ct_students:
-                    break
-                
-                students.append(students.pop(idx))  # pop(0) is O(n) time not O(1)
-            
-            else:
-                students.pop(idx)
-                sandwiches.pop(idx)
-                ct_students -= 1
-                seen = 0
+        from collections import Counter
         
-        return ct_students
+        
+        stud_types = Counter(students)
+        for sand in sandwiches:
+            if sand not in stud_types or not stud_types[sand]:
+                return sum(stud_types.values())
+            
+            stud_types[sand] -= 1
+        
+        return 0
+
 
 
 countStudents = Solution().countStudents
