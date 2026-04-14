dd01ef644509896135c200588d58604173d357e5
Jeffrey <agiwar791005@gmail.com>
2026-04-15 03:05:12 +0800

    https://leetcode.com/problems/first-bad-version/

--- LeetCode/DSA/binary_search/first_bad_version.py
+++ LeetCode/DSA/binary_search/first_bad_version.py
@@ -1,17 +1,45 @@
 # The isBadVersion API is already defined for you.
 # def isBadVersion(version: int) -> bool:
 
+
+bad = 0
+
+
+def isBadVersion(version: int) -> bool:
+    return version >= bad
+
+
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        L = 1
-        R = n
-
-        while L < R:
-            guess_version = (L + R) // 2
-
-            if isBadVersion(guess_version):
-                R = guess_version
+        l_pt, r_pt = 1, n
+        while l_pt < r_pt:
+            m_pt = (r_pt - l_pt) // 2 + l_pt
+            
+            if not isBadVersion(m_pt):
+                l_pt = m_pt + 1
+            
             else:
-                L = guess_version + 1
+                r_pt = m_pt
         
-        return R
+        return r_pt
+
+
+firstBadVersion = Solution().firstBadVersion
+
+def test_firstBadVersion():
+    # LeetCode Example 1: n = 5, bad = 4
+    global bad
+    
+    bad = 4
+    assert firstBadVersion(5) == 4
+
+    # LeetCode Example 2: n = 1, bad = 1
+    bad = 1
+    assert firstBadVersion(1) == 1
+
+    # Edge cases
+
+    print("All tests passed")
+
+if __name__ == "__main__":
+    test_firstBadVersion()
