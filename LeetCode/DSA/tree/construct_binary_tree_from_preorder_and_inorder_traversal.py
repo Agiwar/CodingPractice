5fbc07aff4ce79fe7f9da9285670c5f7160dddc9
Jeffrey <agiwar791005@gmail.com>
2026-05-12 23:31:56 +0900

    https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal

--- LeetCode/DSA/tree/construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ LeetCode/DSA/tree/construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -29,9 +29,43 @@ class Solution:
         root.right = self.buildTree(preorder[(idx + 1):], inorder[(idx + 1):])
         
         return root
+    
+    def buildTreeOpt(self, preorder: list[int], inorder: list[int]) -> TreeNode | None:
+        """
+        using hashmap to store the root's index in inorder, instead of using inorder.index
+            this will get the desired index by O(1) time, no need to traversing list which is O(n)
+
+        define the variable to represent pointer to get current num from preorder,
+            each recursion will increment pointer by one,
+            and avoid slicing the list to get new list, this will save four lists creating,
+            instead, use two pointer left and right to represent the boundary of list
+
+        time = O(n), only traversing preorder
+        space = O(n + h), n is for creating hashmap and tree, h is for recursion call
+        """
+        
+        inorder_idx = {val: idx for idx, val in enumerate(inorder)}
+        self.pt = 0
+        
+        def build(left: int, right: int) -> TreeNode | None:
+            if left > right:
+                return None
+            
+            root = TreeNode(preorder[self.pt])
+            self.pt += 1
+            
+            idx = inorder_idx[root.val]
+            root.left = build(left, idx - 1)
+            root.right = build(idx + 1, right)
+            
+            return root
+        
+        return build(0, len(preorder) - 1)
+
 
 
 buildTree = Solution().buildTree
+buildTreeOpt = Solution().buildTreeOpt
 
 def tree_to_list(root: TreeNode | None) -> list[int | None]:
     """Level-order traversal to list for comparison."""
@@ -61,6 +95,13 @@ def test_buildTree():
     assert tree_to_list(result) == [-1]
 
     # Edge cases
+    # LeetCode Example 1: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7] -> [3,9,20,null,null,15,7]
+    result = buildTreeOpt([3, 9, 20, 15, 7], [9, 3, 15, 20, 7])
+    assert tree_to_list(result) == [3, 9, 20, None, None, 15, 7]
+
+    # LeetCode Example 2: preorder = [-1], inorder = [-1] -> [-1]
+    result = buildTreeOpt([-1], [-1])
+    assert tree_to_list(result) == [-1]
 
     print("All tests passed")
 
