e449336c4c4f2a47e8fa960e543c8bcb5a6e42b0
Jeffrey <agiwar791005@gmail.com>
2026-05-14 23:43:33 +0900

    https://leetcode.com/problems/binary-tree-right-side-view

--- LeetCode/DSA/tree/binary_tree_right_side_view.py
+++ LeetCode/DSA/tree/binary_tree_right_side_view.py
@@ -1,9 +1,7 @@
 from collections import deque
-from typing import List, Optional
 
 
 class TreeNode:
-
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
@@ -11,28 +9,67 @@ class TreeNode:
 
 
 class Solution:
+    def rightSideView(self, root: TreeNode | None) -> list[int]:
+        """
+        implement BFS layer by layer and visit order is from left to right,
+            use queue to store any children of the all node at this layer,
+            after each for looping to visit nodes from queue,
+            just need to record the rightmost node's value.
 
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        queue = deque()
+        enqueue and dequeue are all O(1) time, and need to do n times (n nodes),
+            and access the last one node from queue is O(1) time,
+            and space is O(w) w is max width for typical case,
+                worst case is O(n) for a completely balanced tree,
+                and best case is O(1) for skewed tree,
 
-        if root:
-            queue.append(root)
+        time = O(n)
+        space = O(w), worst case O(n)
+        """
         
+        if not root:
+            return []
+        
+        queue = deque([root])
         result = []
-        while len(queue) > 0:
-
-            level = []
+        
+        while queue:
+            rightmost_val: int | None = None
+            
             for _ in range(len(queue)):
                 curr = queue.popleft()
-                level.append(curr.val)
-
+                rightmost_val = curr.val
+                
                 if curr.left:
                     queue.append(curr.left)
                 
                 if curr.right:
                     queue.append(curr.right)
             
-            val_can_be_seen = level[-1]
-            result.append(val_can_be_seen)
+            result.append(rightmost_val)
         
         return result
+
+
+rightSideView = Solution().rightSideView
+
+def test_rightSideView():
+    # LeetCode Example 1: root = [1,2,3,null,5,null,4] -> [1,3,4]
+    root = TreeNode(1, TreeNode(2, None, TreeNode(5)), TreeNode(3, None, TreeNode(4)))
+    assert rightSideView(root) == [1, 3, 4]
+
+    # LeetCode Example 2: root = [1,2,3,4,null,null,null,5] -> [1,3,4,5]
+    root = TreeNode(1, TreeNode(2, TreeNode(4, TreeNode(5))), TreeNode(3))
+    assert rightSideView(root) == [1, 3, 4, 5]
+
+    # LeetCode Example 3: root = [1,null,3] -> [1,3]
+    assert rightSideView(TreeNode(1, None, TreeNode(3))) == [1, 3]
+
+    # LeetCode Example 4: empty tree -> []
+    assert rightSideView(None) == []
+
+    # Edge cases
+
+    print("All tests passed")
+
+if __name__ == "__main__":
+    test_rightSideView()
