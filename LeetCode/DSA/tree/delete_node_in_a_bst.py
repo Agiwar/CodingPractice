1682c0348e0763c2ff747d33aebac1e4d2358991
Jeffrey <agiwar791005@gmail.com>
2026-08-20 23:23:59 +0800

    https://leetcode.com/problems/delete-node-in-a-bst

--- LeetCode/DSA/tree/delete_node_in_a_bst.py
+++ LeetCode/DSA/tree/delete_node_in_a_bst.py
@@ -31,20 +31,23 @@ class Solution:
         if not root:
             return None
         
-        if root.val > key:
+        elif root.val > key:
             root.left = self.deleteNode(root.left, key)
+        
         elif root.val < key:
             root.right = self.deleteNode(root.right, key)
-        else:
-            if not root.left:
-                return root.right
-            elif not root.right:
-                return root.left
-            else:
-                node = self._find_min(root.right)
-                root.val = node.val
-                root.right = self.deleteNode(root.right, node.val)
         
+        elif not root.left:
+            return root.right
+        
+        elif not root.right:
+            return root.left
+        
+        else:
+            node = self._find_min(root.right)
+            root.val = node.val
+            root.right = self.deleteNode(root.right, node.val)
+
         return root
 
 
